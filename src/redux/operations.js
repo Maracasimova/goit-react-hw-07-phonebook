3f7ce8e1e4a569a80a643f186af9a0725c673ee0
Casimova Marina <maracasimova@gmail.com>
2023-06-26 00:07:20 +0300

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,39 @@
+import * as Api from '../components/API';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const FETCH_CONTACTS = 'contacts/fetch';
+const ADD_CONTACT = 'contact/add';
+const DELETE_CONTACT = 'contact/delete';
+
+export const fetchContacts = createAsyncThunk(FETCH_CONTACTS, async (_, thunkApi) => {
+  try {
+    const data = await Api.getContacts();
+    return data;
+  } catch (error) {
+    return thunkApi.rejectWithValue(error);
+  }
+});
+
+export const addContact = createAsyncThunk(
+  ADD_CONTACT,
+  async (contact, { rejectWithValue }) => {
+    try {
+      const result = await Api.addContact(contact);
+      return result;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  DELETE_CONTACT,
+  async (id, { rejectWithValue }) => {
+    try {
+      await Api.deleteContact(id);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);

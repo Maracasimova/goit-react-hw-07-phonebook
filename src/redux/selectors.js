bc49d309008349dc56ebdcefdec6a5eb9e3ff1b3
Casimova Marina <maracasimova@gmail.com>
2023-06-26 14:11:13 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,5 @@
 export const getContacts = state => state.contacts.items;
 
-export const getFilter = state => state.contacts.filter;
+export const getFilter = state => state.filter;
 
 export const receiveContacts = state => state.contacts.items;
